f9755b938a6e2cbc6c52922f05c9a4f1613c4513
ckettmayer <149213923+ckettmayer@users.noreply.github.com>
2024-12-07 19:57:04 -0300

    Update 3Dtraj_velocity.py

--- 3Dtraj_velocity.py
+++ 3Dtraj_velocity.py
@@ -6,62 +6,59 @@ Created on Wed Nov  1 20:32:46 2023
 @author: ckettmayer
 """
 
-
 ###  3D trajectory instantaneous velocity ###
 
 import numpy as np
-import matplotlib.pyplot as  plt
+import matplotlib.pyplot as plt
 import matplotlib as mpl
-# import cmasher as cmr          #more perceptually uniform colormaps
 
 
-filename = 'outside_3.txt'
-raw_data = open(filename)
-data = np.loadtxt(raw_data, delimiter=None, skiprows=1, usecols = (0,1,2)) #Con usecols elijo que columnas quiero guardar en data
+# Load trajectory data
+filename = 'data_example.txt'
+data = np.loadtxt(filename, delimiter=None, skiprows=1, usecols=(0,1,2)) # Use columns with (x,y,z) data
 
-pixel_size = 0.056   #tamaño de pixel en um
-point_time = 0.032768    #tiempo de adquisición por punto de la trayectoria en s
+# Parameters
+pixel_size = 0.056       #(um)
+point_time = 0.032768    #(s)
 
+# Process coordinates
 x = (data[:,0]- data[0,0])*pixel_size
 y = (data[:,1]- data[0,1])*pixel_size
 z = (data[:,2]- data[0,2])*pixel_size
+t = np.arange(0, len(x)*point_time, point_time)   
 
 
-N = len(x)
-t = np.arange(0, N*point_time, point_time)   #tiempo en segundos
-
-
-v = np.zeros(N-1)
-
-for i in range(N-1):
-    v[i] = np.sqrt( (x[i+1]-x[i])**2  + (y[i+1]-y[i])**2 + (z[i+1]-z[i])**2)/point_time
+# Calculate velocity
+v = np.sqrt(np.diff(x)**2 + np.diff(y)**2 + np.diff(z)**2) / point_time
 
 
 
-
-fig, ax = plt.subplots(subplot_kw=dict(projection='3d'), figsize=(6,4))
+# 3D Plot
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'),figsize=(8,6))
 # ax.view_init(90,270)   #xy plane
+# ax.view_init(0,270)    #xz plane
+# ax.view_init(0,0)      #yz plane
 
-cm = plt.cm.get_cmap('cividis')
-# cm = cmr.bubblegum          
-
+# Colormap
+cm = plt.colormaps['cividis']         
 for i in range(len(v) - 1):
     color = cm((v[i] - min(v)) / (max(v) - min(v)))
     ax.plot(x[i:i+2], y[i:i+2], z[i:i+2], color=color)
     
-
+# Colorbar
 norm = mpl.colors.Normalize(vmin=min(v), vmax=max(v))
 sm = mpl.cm.ScalarMappable(cmap=cm, norm=norm)
 sm.set_array([])
+cbar = fig.colorbar(sm, ax=ax, pad=0.2, shrink=0.7, label=r'Velocity ($\mu$m/s)')
 
-cbar = plt.colorbar(sm, pad=0.3, label='Velocity (um/s)')
-plt.title(filename)
+# Labels and plot adjustments
+plt.title(f'File : {filename}')
+ax.set_xlabel(r'x ($\mu$m)')
+ax.set_ylabel(r'y ($\mu$m)')
+ax.set_zlabel(r'z ($\mu$m)') 
+for pane in [ax.xaxis.pane, ax.yaxis.pane, ax.zaxis.pane]:
+    pane.fill = False
+fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+plt.tight_layout()
 
-plt.xlabel('x (um)')
-plt.ylabel('y (um)')
-ax.set_zlabel('z (um)') 
-ax.xaxis.pane.fill = False
-ax.yaxis.pane.fill = False
-ax.zaxis.pane.fill = False
-
-fig.tight_layout()
+plt.show()
