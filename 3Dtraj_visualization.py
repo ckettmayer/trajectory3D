9ecfda921d8cc946d7f19214bbe768514f6377b2
ckettmayer <149213923+ckettmayer@users.noreply.github.com>
2024-12-07 19:55:40 -0300

    Update 3Dtraj_visualization.py

--- 3Dtraj_visualization.py
+++ 3Dtraj_visualization.py
@@ -11,49 +11,50 @@ Created on Sun Oct 29 19:54:47 2023
 import numpy as np
 import matplotlib.pyplot as  plt
 import matplotlib as mpl
-# import cmasher as cmr      #more perceptually uniform colormaps
 
+# Load trajectory data
+filename = 'data_example.txt'
+data = np.loadtxt(filename, delimiter=None, skiprows=1, usecols=(0,1,2)) # Use columns with (x,y,z) data
 
-filename = 'outside_3.txt'   #SimFCS tracking .txt file
-raw_data = open(filename)
-data = np.loadtxt(raw_data, delimiter=None, skiprows=1, usecols = (0,1,2)) 
- 
+# Parameters 
 pixel_size = 0.056       #(um)
 point_time = 0.032768    #(s)
 
+# Process coordinates
 x = (data[:,0]- data[0,0])*pixel_size
 y = (data[:,1]- data[0,1])*pixel_size
 z = (data[:,2]- data[0,2])*pixel_size
-
-N = len(x)
 t = np.arange(0, N*point_time, point_time)  
 
 
-
-
-fig, ax = plt.subplots(subplot_kw=dict(projection='3d'),figsize=(6,4))
+# 3D Plot
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'),figsize=(8,6))
 # ax.view_init(90,270)   #xy plane
+# ax.view_init(0,270)    #xz plane
+# ax.view_init(0,0)      #yz plane
 
-cm = plt.cm.get_cmap('viridis')
-# cm = cmr.gem                   # CMasher cmap
-
+# Colormap
+cm = plt.colormaps['viridis'] 
 for i in range(len(t)-1):
     ax.plot(x[i:i+2], y[i:i+2], z[i:i+2], color=cm((i+1)/len(t)))
 
 norm= mpl.colors.Normalize(vmin=min(t), vmax= max(t))
 sm = mpl.cm.ScalarMappable(cmap=cm, norm=norm)
 sm.set_array([])
-plt.colorbar(sm, pad=0.2, label='t(s)')
-plt.title(filename)
+cbar = fig.colorbar(sm, ax=ax, pad=0.2, shrink=0.7, label='t (s)')
 
-plt.xlabel('x (um)')
-plt.ylabel('y (um)')
-ax.set_zlabel('z (um)') 
-ax.xaxis.pane.fill = False
-ax.yaxis.pane.fill = False
-ax.zaxis.pane.fill = False
+# Labels and plot adjustments
+plt.title(f'File : {filename}')
+ax.set_xlabel(r'x ($\mu$m)')
+ax.set_ylabel(r'y ($\mu$m)')
+ax.set_zlabel(r'z ($\mu$m)') 
+for pane in [ax.xaxis.pane, ax.yaxis.pane, ax.zaxis.pane]:
+    pane.fill = False
+fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+plt.tight_layout()
 
-fig.tight_layout()
+
+plt.show()
 
 
 
